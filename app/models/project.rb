50ead4c258ef50a9d734a5f59dacee1c387ea20a
Evan Louie <louie.evan@gmail.com>
2014-11-01 15:34:18 -0700

    multithreading enabled & trackable file names added

--- app/models/project.rb
+++ app/models/project.rb
@@ -53,23 +53,26 @@ class Project
     walker.sorting(Rugged::SORT_DATE | Rugged::SORT_REVERSE)    # Sort by date; oldest -> newest
     walker.push(repo.head.target)    # Reference head to point to correct branch
 
+    threads = []
     walker.each do |c|
       t_dir = "public/project_revisions/#{self.id}/#{c.oid}"
       FileUtils.mkdir_p(t_dir)
       FileUtils.cp_r(Dir.glob(dir+"/**"), t_dir) # Use Dir.glob so we don't copy the .git folder
       puts "Rubocop scanning commit: #{c.oid}"
-      rev = self.revisions.new({time: c.time, epoch_time: c.epoch_time, commit_id: c.oid, dir: t_dir, smells: Hash[JSON.parse(%x{rubocop --format json #{t_dir}})["files"].collect { |file| [ file["path"], file["offenses"].length ]}]})
-      root = Dir.pwd
-      Dir.glob(t_dir+"/**/*") do |file|
-        if File.file?(file)
-          count = %x{sed -n '=' #{file} | wc -l}.to_i # System call for line count -> much faster than ruby
-          rev.items.new({name: file, line_count: count, smell_count: rev.smells[file] || 0 })
+      threads << Thread.new {
+        rev = self.revisions.new({time: c.time, epoch_time: c.epoch_time, commit_id: c.oid, dir: t_dir, smells: Hash[JSON.parse(%x{rubocop --format json #{t_dir}})["files"].collect { |file| [ file["path"], file["offenses"].length ]}]})
+        root = Dir.pwd
+        Dir.glob(t_dir+"/**/*") do |file|
+          if File.file?(file)
+            count = %x{sed -n '=' #{file} | wc -l}.to_i # System call for line count -> much faster than ruby
+            rev.items.new({name: file, trackable_name: file.match(/#{c.oid.to_s}\/.*/)[0].gsub(c.oid.to_s, ''), line_count: count, smell_count: rev.smells[file] || 0 })
+          end
         end
-      end
-
+      }
       repo.reset(c.oid, :hard)
     end
     walker.reset
+    threads.each { |thr| thr.join }
     FileUtils.rm_rf(tmp_dir)
   end
 end
